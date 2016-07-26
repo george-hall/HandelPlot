0f835b3006f1cb2aa4363adfa37b3af21fdf4978
George <dyedpurple@googlemail.com>
2016-07-26 13:42:11 +0100

    Moved functionality to main function

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,30 +1,35 @@
 from PIL import Image
 
-x_width = 750
-y_width = 500
+def main():
 
-x_lower = int(x_width * (-2 / 3.0))
-x_upper = int(x_width * (1 / 3.0)) + 1
+    x_width = 750
+    y_width = 500
 
-y_lower = int(y_width * (-1 / 2.0))
-y_upper = int(y_width * (1 / 2.0)) + 1
+    x_lower = int(x_width * (-2 / 3.0))
+    x_upper = int(x_width * (1 / 3.0)) + 1
 
-img = Image.new('RGB', (x_width + 1, y_width + 1), "black")
-pixels = img.load()
+    y_lower = int(y_width * (-1 / 2.0))
+    y_upper = int(y_width * (1 / 2.0)) + 1
 
-for i in xrange(x_lower, x_upper):
-    real_part = i / 100.0
-    for j in xrange(y_lower, y_upper):
-        im_part = j / 100.0
+    img = Image.new('RGB', (x_width + 1, y_width + 1), "black")
+    pixels = img.load()
 
-        z = (0+0j)
-        c = (real_part + (im_part * 1j))
+    for i in xrange(x_lower, x_upper):
+        real_part = i / 100.0
+        for j in xrange(y_lower, y_upper):
+            im_part = j / 100.0
 
-        for k in xrange(64):
-            if z.real > 2 or z.imag > 2:
-                pixels[i + abs(x_lower), j + abs(y_lower)] = (k, k, k)
-                break
-            else:
-                z = (z**2) + c
+            z = (0+0j)
+            c = (real_part + (im_part * 1j))
 
-img.show()
+            for k in xrange(64):
+                if z.real > 2 or z.imag > 2:
+                    pixels[i + abs(x_lower), j + abs(y_lower)] = (k, k, k)
+                    break
+                else:
+                    z = (z**2) + c
+
+    img.show()
+
+if __name__ == "__main__":
+    main()
