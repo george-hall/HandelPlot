eb7041f9e3e02a4270d68e20b64c606cc65d09a2
George <dyedpurple@googlemail.com>
2016-08-01 12:54:16 +0100

    Renamed 'window_*_width' variables
    The name 'window_vert_width' didn't really make any sense... I've
    renamed them to more sensible things now.

--- mandelbrot.py
+++ mandelbrot.py
@@ -22,26 +22,26 @@ def main():
     Set up image canvas and plot the Mandelbrot Set.
     """
 
-    window_horiz_width = 750
-    window_vert_width = 500
+    window_width = 750
+    window_height = 500
 
-    img = create_blank_image(window_horiz_width, window_vert_width)
+    img = create_blank_image(window_width, window_height)
     pixels = img.load()
 
     real_axis_range = (-2, 1) # Min and max on real (x) axis
     im_axis_range = (-1, 1) # Min and max on imaginary (y) axis
 
     # Increase in real component per pixel moved to the right
-    dx = abs(real_axis_range[1] - real_axis_range[0]) / window_horiz_width
+    dx = abs(real_axis_range[1] - real_axis_range[0]) / window_width
 
     # Increase in imaginary component per pixel moved downwards
-    dy = abs(im_axis_range[1] - im_axis_range[0]) / window_vert_width
+    dy = abs(im_axis_range[1] - im_axis_range[0]) / window_height
 
     real_part = real_axis_range[0]
 
-    for x_pixel in xrange(window_horiz_width):
+    for x_pixel in xrange(window_width):
         im_part = im_axis_range[0]
-        for y_pixel in xrange(window_vert_width):
+        for y_pixel in xrange(window_height):
 
             z_value = (0+0j)
             c_value = (real_part + (im_part * 1j))
